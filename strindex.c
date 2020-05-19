d340b9c4f498895be240e745748fd3f2e36d8c3a
berkeyvx <berkeyvx@gmail.com>
2020-05-19 15:53:45 +0300

    strindex.c pushed wrong code, returns position of rightmost occurence

--- strindex.c
+++ strindex.c
@@ -1,6 +1,6 @@
 /*
 Write the function strindex(s,t) which returns the position of the rightmost
-occurrence of t in s, or -1 if there is none. 
+occurrence of t in s, or -1 if there is none.
 */
 
 #include <stdio.h>
@@ -10,26 +10,35 @@ int strindex(char s[], char t[]);
 
 int main(int argc, char const *argv[])
 {
-	char *s = "abcdedfabcde";
-	char *t = "abc";
-	printf("%d",strindex(s,t));
-	return 0;
+    char *s = "abcdefghh";
+    char *t = "fgh";
+    char *s2 = "abcdefabcde";
+    char *t2= "abc";
+    printf("%d\n",strindex(s,t));
+    printf("%d\n",strindex(s2, t2));
+    return 0;
 }
 
 int strindex(char s[], char t[]){
-	int pos;
-	int i,j,k;
-	int sLen = strlen(s);
-	int tLen = strlen(t);
-	for(i = sLen - 1; i >= 0; --i){
-		k = i;
-		for(j = tLen -1; j >= 0; --j){
-			if(s[k--] != t[j]){
-				break;
-			}
-			return i-tLen;
-		}
-	}
+    int pos = 0;;
+    int i,j,k;
+    long sLen = strlen(s); // strlen returns long need coercion
+    long tLen = strlen(t);
+    for(i = sLen - 1; i >= 0; --i){
+        k = i;
+        for(j = tLen -1; j >= 0; --j){
+            if(s[k--] != t[j]){
+                pos = 0;
+                break;
+            }
+            else{
+                pos++;
+            }
+        }
+        if(pos == tLen){
+            return i - tLen + 1;
+        }
+    }
 
-	return -1;
+    return -1;
 }
